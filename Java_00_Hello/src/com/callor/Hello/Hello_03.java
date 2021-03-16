3582a408159dcf173ff00120a29173a561ebc383
Eungyeol41 <iniziato@naver.com>
2021-03-16 15:57:46 +0900

    Nums02 추가

---
+++ Java_00_Hello/src/com/callor/Hello/Hello_03.java
@@ -0,0 +1,32 @@
+package com.callor.Hello;
+
+/*
+ * 숫자 연산
+ * java에서는 수학의 사칙연산과 나머지 연산을  수행할 수 있다.
+ * 
+ * 숫자가 정수형일 때는 사칙연산의 결과가 모두 정수이다
+ * 
+ * 정수 / 정수 연산을 수행했을 때 결과는 나눗셈의 몫을 구하는 연산이 된다
+ * 정수 % 정수 연산을 수행하면 결과는 나머지를 구하는 연산이 된다
+ * 
+ */
+
+public class Hello_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+		System.out.println(1 % 2);
+		System.out.println(2 % 2);
+		System.out.println(3 % 2);
+		System.out.println(4 % 2);
+		System.out.println(5 % 2);
+	
+		
+	}
+
+}
---
+++ Java_00_Hello/src/com/callor/Hello/Nums02.java
@@ -0,0 +1,10 @@
+package com.callor.Hello;
+
+public class Nums02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+	}
+
+}
