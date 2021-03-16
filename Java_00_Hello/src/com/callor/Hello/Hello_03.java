736cfcafc3ecc60b8b95ab815eacb57bae2e802a
Eungyeol41 <iniziato@naver.com>
2021-03-16 16:02:01 +0900

    Hello_03 추가

--- Java_00_Hello/src/com/callor/Hello/Hello_03.java
+++ Java_00_Hello/src/com/callor/Hello/Hello_03.java
@@ -25,7 +25,17 @@ public class Hello_03 {
 		System.out.println(3 % 2);
 		System.out.println(4 % 2);
 		System.out.println(5 % 2);
-	
+		
+		System.out.println("-----------");
+		// 40을 30으로 나누면 일반적 수학 연산에서는 소수점 이하 값이 나타나지만
+		// java에서 정수 / 정수는 몫을 구하기 때문에 결과는 1만 나타난다
+		System.out.println(40 / 30);
+		
+		// java 에서 실수의 표현
+		// java는 실수를 표현할 때 10^-16까지 표현한다
+		// 이후 값은 무시 또는 반올림 등을 수행한다
+		System.out.println(40.0 / 30.0);
+		
 		
 	}
 
