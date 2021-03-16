b2bb57d0453f2aeb3d845e53a229ed439fff01f1
Eungyeol41 <iniziato@naver.com>
2021-03-16 15:14:26 +0900

    Hello_02 삭제

--- Java_00_Hello/src/com/callor/Hello/Hello_02.java
+++ Java_00_Hello/src/com/callor/Hello/Hello_02.java
@@ -3,9 +3,7 @@ package com.callor.Hello;
 public class Hello_02 {
 
 	public static void main(String[] args) {
-		System.out.println("반갑습니다");
-		System.out.println(30 + 40);
-		System.out.println(30 * 40);
+		
 
 	}
 
