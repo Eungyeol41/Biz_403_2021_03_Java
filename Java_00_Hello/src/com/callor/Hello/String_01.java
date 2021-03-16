46efc0d258b343d0c3efe3dfde69e604fe6bd246
Eungyeol41 <iniziato@naver.com>
2021-03-16 16:22:55 +0900

    Hello_03 추가

---
+++ Java_00_Hello/src/com/callor/Hello/String_01.java
@@ -0,0 +1,19 @@
+package com.callor.Hello;
+
+public class String_01 {
+
+	public static void main(String[] args) {
+		
+		// 문자열
+		// String형 값 (데이터, 자료)
+		// String형 리터럴
+		System.out.println("대한민국");
+		System.out.println("우리나라");
+		System.out.println("30 + 40");
+		System.out.println("30 + 40 = 70");
+		
+		
+
+	}
+
+}
