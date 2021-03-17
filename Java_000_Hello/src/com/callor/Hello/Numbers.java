4fe560c8b7cc3ecff4432fbc79010e7b545468c5
Eungyeol41 <iniziato@naver.com>
2021-03-17 10:06:10 +0900

    Numbers 연산복습

--- Java_000_Hello/src/com/callor/Hello/Numbers.java
+++ Java_000_Hello/src/com/callor/Hello/Numbers.java
@@ -1,5 +1,15 @@
 package com.callor.Hello;
 
+
+/*
+ * ===============
+ * 33 + 2 =
+ * 33 - 2 =
+ * 33 * 2 =
+ * 33 / 2 =
+ * 33 % 2 =
+ * ================
+ */
 public class Numbers {
 
 	public static void main(String[] args) {
