d380d3209d2cdfbdb8f1eef01779c9e331a6dee6
Eungyeol41 <iniziato@naver.com>
2021-03-17 10:30:41 +0900

    Java_001_Value 추가

---
+++ Java_001_Value/src/com/collar/values/Values_02.java
@@ -0,0 +1,13 @@
+package com.collar.values;
+
+public class Values_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1. 반지름이 23인 원의 둘레와 넓이를 계산하여 출력");
+		System.out.println("원의 둘레 : 23 x 2 x 3.14 = " + (23 * 2 * 3.14));
+		System.out.println("원의 넓이 : 23 x 23 x 3.14 = " + (23 * 23 * 3.14));
+
+	}
+
+}
