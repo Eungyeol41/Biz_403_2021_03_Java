ac79e0b81e84ef224816895a7c6469cbaf49f453
Eungyeol41 <iniziato@naver.com>
2021-03-17 09:07:53 +0900

    Numbers 추가

---
+++ Java_00_Hello/src/com/callor/Hello/Numbers.java
@@ -0,0 +1,14 @@
+package com.callor.Hello;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "33 + 2 = " + 35);
+		System.out.println("33 - 2 = " + 31);
+		System.out.println("33 * 2 = " + 66);
+		System.out.println("33 / 2 = " + 16);
+		System.out.println("33 % 2 = " + 1);
+	}
+
+}
