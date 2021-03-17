2a7881a8086cc3c7e49f770449ffdf0f93d8c1ad
Eungyeol41 <iniziato@naver.com>
2021-03-17 10:51:51 +0900

    Values_02 코드 추가

---
+++ Java_001_Value/src/com/collar/values/Print_01.java
@@ -0,0 +1,14 @@
+package com.collar.values;
+
+public class Print_01 {
+
+	public static void main(String[] args) {
+		
+		// Console에 출력하는 명령문
+		/*
+		 * 비어있는 라인(Enter)만 출력하라
+		 */
+		System.out.println();
+	}
+
+}
--- Java_001_Value/src/com/collar/values/Values_02.java
+++ Java_001_Value/src/com/collar/values/Values_02.java
@@ -7,6 +7,15 @@ public class Values_02 {
 		System.out.println("1. 반지름이 23인 원의 둘레와 넓이를 계산하여 출력");
 		System.out.println("원의 둘레 : 23 x 2 x 3.14 = " + (23 * 2 * 3.14));
 		System.out.println("원의 넓이 : 23 x 23 x 3.14 = " + (23 * 23 * 3.14));
+		System.out.println("=======================================================");
+		
+		System.out.println("원의 둘레와 넓이");
+		System.out.print("원의 둘레 : ");
+		System.out.print("23 x 2 x 3.14 = ");
+		System.out.println(23 * 2 * 3.14);
+		System.out.print("원의 넓이 : ");
+		System.out.print("23 x 23 x 3.14 = ");
+		System.out.println(23 * 23 * 3.14);
 
 	}
 
