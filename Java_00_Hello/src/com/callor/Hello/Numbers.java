8aa7db0202fdffce8295f0bd9f0100e9e16d1dd0
Eungyeol41 <iniziato@naver.com>
2021-03-17 09:14:07 +0900

    Numbers 코드 추가

--- Java_00_Hello/src/com/callor/Hello/Numbers.java
+++ Java_00_Hello/src/com/callor/Hello/Numbers.java
@@ -4,11 +4,11 @@ public class Numbers {
 
 	public static void main(String[] args) {
 		
-		System.out.println( "33 + 2 = " + 35);
-		System.out.println("33 - 2 = " + 31);
-		System.out.println("33 * 2 = " + 66);
-		System.out.println("33 / 2 = " + 16);
-		System.out.println("33 % 2 = " + 1);
+		System.out.println( "33 + 2 = " + (33 + 2));
+		System.out.println("33 - 2 = " + (33 - 2));
+		System.out.println("33 * 2 = " + (33 * 2));
+		System.out.println("33 / 2 = " + (33 / 2));
+		System.out.println("33 % 2 = " + (33 % 2));
 	}
 
 }
