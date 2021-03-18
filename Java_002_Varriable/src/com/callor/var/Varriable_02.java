b95e719b040edaa3637e1617119b9abfcd1c0252
Eungyeol41 <iniziato@naver.com>
2021-03-18 09:50:27 +0900

    변수 추가

---
+++ Java_002_Varriable/src/com/callor/var/Varriable_02.java
@@ -0,0 +1,40 @@
+package com.callor.var;
+
+public class Varriable_02 {
+	
+	public static void main(String[] args) {
+		
+		// 정수형 변수 선언과 clear
+		// 정수형 변수 intNum1, intNum2 생성
+		int intNum1 = 0; // 기본형
+		Integer intNum2 = 0; // 확장형
+		
+		// 실수형 변수 floatNum1, floatNum2 생성
+		// 기억장치를 소모하는 용량이 double보다 작다
+		// 대신 정밀도는 낮다
+		float floatNum1 = 0.0f;
+		float floatNum2 = 0.0F;
+		
+		// 실수형 변수 선언과 clear
+		// 실수형 변수 douNum1, douNum2 생성
+		double douNum1 = 0.0;
+		double douNum2 = 0.0;
+		
+		// 정수형 변수 longNum1, longNum2 생성
+		// int, Integer형보다 기억장치 소모 용량이 크다
+		// int형은 최대 2^32만큼 공간
+		// long형은 최대 2^64만큼 공간
+		long longNum1 = 0;
+		long longNum2 = 0;
+		
+		// 문자열형 변수를 선언하고 clear
+		// 문자"열"형 변수 strName을 생성
+		String strName = ""; // " "
+		String strName = null;
+		
+		
+		
+		
+	}
+
+}
