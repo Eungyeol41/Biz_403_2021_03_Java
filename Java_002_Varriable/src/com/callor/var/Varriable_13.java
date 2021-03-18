cee983b9f2a3d91bb26aee861d8fc86a352b2be1
Eungyeol41 <iniziato@naver.com>
2021-03-18 14:39:09 +0900

    Varriable 추가

---
+++ Java_002_Varriable/src/com/callor/var/Varriable_13.java
@@ -0,0 +1,22 @@
+package com.callor.var;
+
+public class Varriable_13 {
+	
+	public static void main(String[] args) {
+		
+		int intNum = 0;
+		int intSum = 0;
+		
+		// 1. intNum 변수에 2를 추가하라 --> intNum == 2
+		// 2. intSum에 intNum 변수에 저장된 값을 읽어서 추가하라 --> intSum == 2
+		intSum += (intNum += 2 );
+		
+		intSum += (intNum += 5);
+		// ?? intSum
+				
+		intSum += (intNum += 2);
+		// ?? intSum 
+		
+	}
+
+}
---
+++ Java_002_Varriable/src/com/callor/var/Varriable_15.java
@@ -0,0 +1,20 @@
+package com.callor.var;
+
+public class Varriable_15 {
+
+	public static void main(String[] args) {
+		
+		int intNum = 0;
+		int intSum = 0;
+		
+		intSum += (intNum += 2);
+		intSum += (intNum += 2);
+		intSum += (intNum += 2);
+		intSum += (intNum += 2);
+		intSum += (intNum += 2);
+		System.out.println(intSum);
+		
+		
+	}
+
+}
