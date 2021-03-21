01ac71b403bbdfbc17156621311e30f35c9a38df
Eungyeol41 <iniziato@naver.com>
2021-03-21 21:28:42 +0900

    복습

--- Java_003_Control/src/com/callor/controller/Cont_08A.java
+++ Java_003_Control/src/com/callor/controller/Cont_08A.java
@@ -4,11 +4,6 @@ public class Cont_08A {
 
 	public static void main(String[] args) {
 		
-//		int intNum = 77;
-//		for(int i = 2; i > intNum -1; i++) {
-//			System.out.println(intNum % i == 0);
-//			break;
-//		}
 		int intNum = 79;
 		for(int i = 2; i < intNum ; i++) {
 			boolean bYes = intNum % i == 0;
